2ac8e54fc21cea1fc53635c282247ec85f99d2f6
pravin7878 <143276463+pravin7878@users.noreply.github.com>
2024-03-08 16:12:58 +0530

    updet

--- src/AllRoutes/PrivateRoutes.jsx
+++ src/AllRoutes/PrivateRoutes.jsx
@@ -1,17 +1,19 @@
 import { useAuth0 } from '@auth0/auth0-react'
-import React from 'react'
+import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { Allcontext } from '../ContextAPI/ContextProvider'
 
 export default function PrivateRoutes({children}) {
+  const {Authstate} = useContext(Allcontext)
   const nevigate = useNavigate()
  const {isAuthenticated}  =  useAuth0()
 
  
-  let setTime = setTimeout(()=>{
-    return nevigate('/login')
-   },2000)
+  // let setTime = setTimeout(()=>{
+  //   return nevigate('/login')
+  //  },2000)
   
  
 
-  return isAuthenticated ? children : setTime
+  return Authstate ? children : nevigate('/login')
 }
--- src/ContextAPI/ContextProvider.jsx
+++ src/ContextAPI/ContextProvider.jsx
@@ -1,11 +1,14 @@
 import React, { createContext, useEffect, useReducer, useState } from 'react'
 import { useToast } from '@chakra-ui/react'
+import { useAuth0 } from '@auth0/auth0-react'
 
 export const Allcontext = createContext()
 
 
 export default function ContextProvider({ children }) {
-  // const [Authstate, setAuthstate] = useState({ isAuth: false, token: null })
+  const {isAuthenticated}  =  useAuth0()
+  const [Authstate, setAuthstate] = useState(isAuthenticated)
+
   const [cartItems, setcartItems] = useState([])
   const [cartitemID, setcartitemID] = useState(0)
 const [quantity,setquantity] = useState(1)
@@ -67,71 +70,9 @@ const addTocart = (productdetail)=>{
   })
 }
  
-// function  addquantity (ind,value){
-//   cartitem[ind].quantity+1
-  
-// }
-
-  // const quantityIncrease = (id) => {
-  //   state.cartItems.filter((ele,ind)=>{
-  //     if(id===ind){
-  //       console.log(ele.quantity);  
-  //       return ele.setquantity(quantity+1)
-  //     }
-  //   })
-//      if()
-// setquantity((q)=>q+1)
-// console.log(cartitem[index].quantity+1);
-        // console.log(cartitem[index].price * 2);
-        // return quantity < 5 ? setquantity(quantity + 1) : null
-    // }
-//     const quantityDecrease = (index,e) => {
-//       e.preventDefault()
-//         // return quantity > 1 ? setquantity(quantity - 1) : null
-// console.log(cartitem[index]);
-
-    // }
-
-
-
-
-  // const addTocart = (item) => {
-  //   // const { data, quantity } = item
-  //   // const { data} = item
-  //   let stordProductID  = cartItems.find((cartItem) => cartItem.id === item.id);
-  //   console.log(stordProductID);
-  //   if (!stordProductID) {
-  //     // setcartitemID(data.id)
-  //     const newArray = [...cartItems, item];
-  //     setcartItems(newArray);
-  //     localStorage.setItem('cartdata', JSON.stringify(newArray));
-  //     toast({
-  //       title: 'Item is Success Fully Added To Cart',
-  //       position: 'top',
-  //       isClosable: true,
-  //       status: 'success'
-  //     })
-  //   }
-
-    // else {
-    //   toast({
-    //     title: 'Item is alredy Added To Cart',
-    //     position: 'top',
-    //     isClosable: true,
-    //     status: 'warning'
-    //   })
-    // }
-  // };
-
-  // useEffect(() => {
-  //   const storedArray = JSON.parse(localStorage.getItem('cartdata'));
-  //   if (storedArray) {
-  //     setcartItems(...state.cart, storedArray);
-  //   }
-  // }, []);
 
   return (
-    <Allcontext.Provider value={{ quantity, addTocart,state,}}>
+    <Allcontext.Provider value={{ quantity, addTocart,state,Authstate}}>
       {children}
     </Allcontext.Provider>
   )
