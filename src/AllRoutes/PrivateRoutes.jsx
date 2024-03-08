07cb40b4a3f5dcfb105901c715afdc507535d7a2
pravin7878 <143276463+pravin7878@users.noreply.github.com>
2024-03-08 16:24:33 +0530

    Update PrivateRoutes.jsx

--- src/AllRoutes/PrivateRoutes.jsx
+++ src/AllRoutes/PrivateRoutes.jsx
@@ -2,18 +2,32 @@ import { useAuth0 } from '@auth0/auth0-react'
 import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Allcontext } from '../ContextAPI/ContextProvider'
+import { useToast } from '@chakra-ui/react'
 
 export default function PrivateRoutes({children}) {
   const {Authstate} = useContext(Allcontext)
   const nevigate = useNavigate()
  const {isAuthenticated}  =  useAuth0()
-
+let toast = useToast()
  
   // let setTime = setTimeout(()=>{
   //   return nevigate('/login')
   //  },2000)
   
+  const chackstate  = ()=>{
+    toast({
+      title:'Please Login Now',
+      status:'warning',
+      isClosable:true,
+      position:'top'
+    })
+    return(
+      nevigate('/')
+     
+    )
+    
+  }
  
 
-  return Authstate ? children : nevigate('/login')
+  return isAuthenticated ? children : {chackstate}
 }
